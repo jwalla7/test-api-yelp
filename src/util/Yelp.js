a2533cdd6261c1ce1c386043cdf24d2a7969797e
jwalla7 <j7wallace7@outlook.com>
2022-03-23 11:46:39 -0500

    mar 23 1146

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,7 @@ const apiKey = 'oJKT4v8jkExN9P3HLjUqvpQYbv06tFCbt0O0UDVCKcY80aC2WFUVXX8kgRMlsYNl
 
 const Yelp = {
     searchYelp(term, location, sortBy) {
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}}&location=${location}&sort_by=${sortBy}`,
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}}&location=${location}&sort_by=${sortBy}`,
         {
             headers: {
                 Authorization: `Bearer ${apiKey}`,
